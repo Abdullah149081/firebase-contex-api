f073eb0827347fac5a8267ee5eaaf7c399c411fc
Abdullah149081 <masudmd425@gmail.com>
2023-04-26 11:48:35 +0600

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    // "extends:eslint:recommended",
+  ],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "no-unused-vars": 1,
+    "no-undef": 0,
+    "react/react-in-jsx-scope": 0,
+    "react-hooks/rules-of-hooks": "error",
+    "no-console": 0,
+    "react/state-in-constructor": 0,
+    indent: 0,
+    "linebreak-style": 0,
+    "react/prop-types": 0,
+    "jsx-a11y/click-events-have-key-events": 0,
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
